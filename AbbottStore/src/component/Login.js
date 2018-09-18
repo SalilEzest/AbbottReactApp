d8acae8b784d4ad98e5599cc4415587cbb91888b
salil <salil.luley@gmail.com>
2018-09-19 00:32:20 +0530

    integrated native base library, login component created with static UI

---
+++ AbbottStore/src/component/Login.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {View, Container, Text, Header, Content, Form, Item, Input, Button, Label } from 'native-base';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange'
+  },
+  button: {
+    flex: 0.9,
+    justifyContent: 'center'
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+});
+
+export default class FloatingLabelExample extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Container>
+          <Header>
+            <Text>
+                Login Page
+            </Text>
+          </Header>
+          <Content>
+            <Form>
+              <Item floatingLabel>
+                <Label>Username</Label>
+                <Input />
+              </Item>
+              <Item floatingLabel last>
+                <Label>Password</Label>
+                <Input />
+              </Item>
+            </Form>
+            <View padder style={styles.buttons}>
+              <Button rounded light style={styles.button}>
+                <Text> Login </Text>
+              </Button>
+              <Button rounded light>
+                <Text> Forgot Password </Text>
+              </Button>
+            </View>
+          </Content>
+        </Container>
+      </SafeAreaView>
+    );
+  }
+}
